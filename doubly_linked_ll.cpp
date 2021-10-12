07dc1d8d1c74a99f294eb4e41aa8cf7cd287a5b4
Aabid Farooq <74071947+aab1d@users.noreply.github.com>
2021-10-13 00:06:07 +0530

    Create doubly_linked_ll.cpp

---
+++ doubly_linked_ll.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+class Node{
+    int val;
+    Node *next;
+    Node *prev;
+public:
+    void set_val(int data){ val = data; }
+    int get_val(){ return val; }
+    void set_next(Node *n){ next = n; }
+    Node* get_next(){ return next; }
+    void set_prev(Node *p){ prev = p;}
+    Node* get_prev(){ return prev; }
+};
+class LinkedList{
+    Node *head = NULL;
+    Node *tail = NULL;
+public:
+    void create(){
+        int n;      //number of elements
+        cout<<"Limit: ";
+        cin>>n;
+        for(int i = 0; i < n; i++){
+            int data;
+            cout<<"Data: ";
+            cin>>data;
+            
+            Node *tmp = new Node();
+            tmp->set_val(data);
+            tmp->set_next(NULL);
+            tmp->set_prev(NULL);
+            
+            if(i == 0){
+                head = tmp;
+                tail = tmp;
+            }
+            else{
+                tail->set_next(tmp);
+                tmp->set_prev(tail);
+                tail = tmp;
+            }
+        }
+    }
+    void view(){
+        Node *trav = head;
+        cout<<"List: ";
+        while(trav != NULL){
+            cout<<trav->get_val()<<" ";
+            trav = trav->get_next();
+        }
+        cout<<endl;
+    }
+};
+int main()
+{
+    LinkedList l;
+    l.create();
+    l.view();
+    return 0;
+}
